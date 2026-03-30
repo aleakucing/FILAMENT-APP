4e5fbb84919667855f5aa80a86e4040b2b8f87f6
ibrqp <mibrahimalamin98@gmail.com>
2026-03-30 20:35:00 +0700

    Jobsheet6_Praktikum6

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Posts\Schemas;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Select;
@@ -21,32 +22,71 @@ class PostForm
         return $schema
             ->components([
 
+                // LEFT (2/3)
                 Section::make("Post Details")
-                ->schema([
-                       Group::make([
-                            TextInput::make('title'),
-                            TextInput::make('slug'),
-                            Select::make('category_id'),
+                    ->icon("heroicon-o-document-text")
+                    ->description("Isi data utama post")
+                    ->schema([
+
+                        // 2 kolom field utama
+                        Group::make([
+                            TextInput::make('title')
+                                ->rules('required | min:5 | max:10')
+                                ->validationMessages([
+                                    'min' => 'Title harus lebih dari 5.',
+                                ]),
+                            // ->minLength(5),
+
+                            TextInput::make('slug')
+                                ->required()
+                                ->minLength(3)
+                                ->unique()
+                                ->validationMessages([
+                                    'unique' => 'Slug harus unik dan tidak boleh sama.',
+                                ]),
+
+                            Select::make('category_id')
+                                ->relationship('category', 'name')
+                                ->searchable()
+                                ->preload()
+                                ->required(),
+
                             ColorPicker::make('color'),
-                        MarkdownEditor::make('body')
+                        ])->columns(2),
+
+                        // full width editor
+                        MarkdownEditor::make('content')
                             ->columnSpanFull(),
-                    ])->columns(2),
-                MarkdownEditor::make('content'),
-                ])->columnSpan(2),
+
+                    ])
+                    ->columnSpan(2),
+
+                // RIGHT (1/3)
                 Group::make([
-                    Section::make('Image Upload')
+
+                    Section::make("Image Upload")
+                        ->icon("heroicon-o-photo")
                         ->schema([
                             FileUpload::make('image')
+                                ->image()
                                 ->disk('public')
-                                ->directory('post'),
+                                ->required()
+                                ->directory('posts'),
+                                
                         ]),
-                    Section::make('Meta')
+
+                    Section::make("Meta Information")
+                        ->icon("heroicon-o-cog-6-tooth")
                         ->schema([
                             TagsInput::make('tags'),
                             Checkbox::make('published'),
-                            DatePicker::make('published_at'),
+                            DateTimePicker::make('published_at'),
                         ]),
-                ])->columnSpan(1)
-            ])->columns(3);
+
+                ])
+                    ->columnSpan(1),
+
+            ])
+            ->columns(3);
     }
 }

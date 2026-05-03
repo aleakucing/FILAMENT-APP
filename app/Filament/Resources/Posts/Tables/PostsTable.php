e1a13d1cfb8bbdbe51f7a03fd762647a3b702379
ibrqp <mibrahimalamin98@gmail.com>
2026-05-03 23:52:44 +0700

    Jobsheet10_Praktikum13

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\Posts\Tables;
 
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ReplicateAction;
 use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\IconColumn;
@@ -15,6 +18,7 @@ use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 use function Laravel\Prompts\search;
+use Filament\Forms\Components\Checkbox;
 
 class PostsTable
 {
@@ -76,7 +80,23 @@ class PostsTable
                     ->preload(),
             ])
             ->recordActions([
+                ReplicateAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
+
+                Action::make('status')
+                    ->label('Status Change')
+                    ->icon('heroicon-o-check-circle')
+                    ->form([
+                        Checkbox::make('published')
+                            ->default(fn($record): bool => $record->published),
+                    ])
+                    ->action(function ($record, $data) {
+                        $record->update([
+                            'published' => $data['published'],
+                        ]);
+                    }),
+                    
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
